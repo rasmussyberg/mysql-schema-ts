dc21e4b72fb0b907b60609af729690af8556e3d1
Rasmus Syberg <rvs@brickshare.io>
2021-04-29 14:52:03 +0200

    chore: Not working well with tinyints
    I will revert back to boolean for tinyint.
    I will add switches to config this soon.

--- src/column-map.ts
+++ src/column-map.ts
@@ -45,7 +45,6 @@ function findTsType(udtName: string): string | null {
       return 'string'
     case 'integer':
     case 'int':
-    case 'tinyint':
     case 'smallint':
     case 'mediumint':
     case 'bigint':
@@ -55,6 +54,8 @@ function findTsType(udtName: string): string | null {
     case 'float':
     case 'year':
       return 'number'
+    case 'tinyint':
+      return 'boolean'
     case 'json':
       return 'JSONValue'
     case 'date':
